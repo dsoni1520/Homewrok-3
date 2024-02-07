92aa1b4bb52be15c3a924c0cca1d9e84fe695d4b
dsoni1520 <112735668+dsoni1520@users.noreply.github.com>
2024-02-07 12:29:04 -0500

    Delete test_answer.py

--- test_answer.py
+++
@@ -1,8 +0,0 @@
-import answer
-
-def test_add():
-    assert (answer.add(1, 2) == 3)
-    assert (answer.add(1, 1) == 2)
-    assert (answer.add(100, 200) == 300)
-    assert (answer.add(0, 1) == 1)
-    assert (answer.add(-1, 1) == 0)
