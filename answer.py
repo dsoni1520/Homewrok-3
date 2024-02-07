2134487c64ae3e68493e5d926f8cf73d49b990aa
dsoni1520 <112735668+dsoni1520@users.noreply.github.com>
2024-02-07 12:28:11 -0500

    Initial commit

---
+++ answer.py
@@ -0,0 +1,17 @@
+def hello_world():
+    # Write a doc string below. Doc strings can be used to provide the user with a
+    # description of what the function does.
+    """
+
+    """
+
+    # Write a comment; comments are very useful to other programmers.
+
+    # Write a Python statement to print Hello, World!
+
+def add(a,b):
+
+    # Write simple code to add variable a and b
+    output =
+
+    return output
